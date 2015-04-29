1c2496bd2fad903e9b9752a615fa9fc7ba9d6f9a
Bart <siniar1990@gmail.com>
2015-04-29 12:49:38 +0100

    fourth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Logentries::Application.routes.draw do
 
+  resources :posts
+
   root "welcome#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
